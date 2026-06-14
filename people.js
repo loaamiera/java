49fa86c748fc2856b3095fdfd67b84d38bd051a5
loaamiera <brownamiera@gmail.com>
2026-06-13 19:23:02 -0700

    Add fetch request to Google People API

--- people.js
+++ people.js
@@ -1,1 +1,15 @@
+const accessToken = ${key};
 
+fetch(
+  "https://people.googleapis.com/v1/{id}/personFields=${fields}",
+  {
+    method: "GET",
+    headers: {
+      "Authorization": `Bearer ${accessToken}`,
+      "Accept": "application/json"
+    }
+  }
+)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
